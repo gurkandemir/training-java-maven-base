757fa88fe8693d1ccb23b663c9e16249e6ba2ae4
gurkan.demir <gurkan.demir@trendyol.com>
2022-06-09 15:57:43 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/com/codurance/base/StringCalculatorShould.java

--- src/test/java/com/codurance/base/StringCalculatorShould.java
+++ src/test/java/com/codurance/base/StringCalculatorShould.java
@@ -1,53 +1,38 @@
 package com.codurance.base;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class StringCalculatorShould {
 
-    @Test
-    void return_0_when_empty_string_given() {
+    @ParameterizedTest
+    @MethodSource(value = "inputsForCalculator")
+    void return_summation_according_to_input(String input, int expected) {
+        // given
         StringCalculator calculator = new StringCalculator();
-        assertEquals(0, calculator.Add(""));
+
+        // when
+        int result = calculator.Add(input);
+
+        // then
+        assertEquals(expected, result);
     }
 
-
-    @Test
-    void return_1_when_the_input_string_is_1() {
-        StringCalculator calculator = new StringCalculator();
-        assertEquals(1, calculator.Add("1"));
+    private static Stream<Arguments> inputsForCalculator() {
+        return Stream.of(
+            Arguments.of("", 0),
+            Arguments.of("1", 1),
+            Arguments.of("2", 2),
+            Arguments.of("1,2", 3),
+            Arguments.of("1,2,3", 6),
+            Arguments.of("3,4\n5", 12),
+            Arguments.of("3\n4\n5", 12),
+            Arguments.of("3\n4,5", 12)
+        );
     }
-
-    @Test
-    void return_3_when_the_input_string_is_1_comma_2() {
-        StringCalculator calculator = new StringCalculator();
-        assertEquals(3, calculator.Add("1,2"));
-    }
-
-    @Test
-    void return_6_when_the_input_string_is_1_comma_2_comma_3() {
-        StringCalculator calculator = new StringCalculator();
-        assertEquals(6, calculator.Add("1,2,3"));
-    }
-
-    @Test
-    void return_12_when_the_string_is_3_comma_4_backslashN_5() {
-        StringCalculator calculator = new StringCalculator();
-        assertEquals(12, calculator.Add("3,4\n5"));
-    }
-
-    @Test
-    void return_12_when_the_string_is_3_backslashN_4_backslashN_5() {
-        StringCalculator calculator = new StringCalculator();
-        assertEquals(12, calculator.Add("3\n4\n5"));
-    }
-
-    @Test
-    void return_12_when_the_string_is_3_backslashN_4_comma_5() {
-        StringCalculator calculator = new StringCalculator();
-        assertEquals(12, calculator.Add("3\n4,5"));
-    }
-
 }
