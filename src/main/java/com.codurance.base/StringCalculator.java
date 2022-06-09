9a9df6d18b2e5a2c977cb5737ebe32e9869a7058
gurkan.demir <gurkan.demir@trendyol.com>
2022-06-09 16:38:44 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/com.codurance.base/StringCalculator.java

--- src/main/java/com.codurance.base/StringCalculator.java
+++ src/main/java/com.codurance.base/StringCalculator.java
@@ -1,5 +1,7 @@
 package com.codurance.base;
 
+import java.util.List;
+
 public class StringCalculator {
     int Add(String numbers) throws NegativeInputException {
         if ("".equals(numbers))
@@ -10,12 +12,18 @@ public class StringCalculator {
         String delimeter = findDelimeter(numbers);
         String[] nums = getNumbers(numbers).split(delimeter+"|\n");
 
+        List<Integer> negativeNumbers = new java.util.ArrayList<Integer>(List.of());
+
         for(String number: nums){
             int num = Integer.parseInt(number);
             if(num<0){
-                throw new NegativeInputException("Negative values: " + num);
+                negativeNumbers.add(num);
             }
             result += Integer.parseInt(number);
+        }
+
+        if (negativeNumbers.size() > 0) {
+            throw new NegativeInputException("Negative values: " + prepareMessageForNegativeValues(negativeNumbers));
         }
 
         return result;
@@ -41,5 +49,16 @@ public class StringCalculator {
         }
 
         return input;
+    }
+
+    String prepareMessageForNegativeValues(List<Integer> negativeNumbers) {
+        StringBuilder message = new StringBuilder();
+
+        for (int number : negativeNumbers) {
+            message.append(number).append(",");
+        }
+
+        return message.substring(0, message.length() -1);
+
     }
 }
