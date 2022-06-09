306ba76b128fdd241e2c4de1fa2d62d3d9707c7c
Mert Bilgiç <mert.bilgic@trendyol.com>
2022-06-09 15:21:33 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/com/codurance/base/StringCalculatorShould.java

--- src/test/java/com/codurance/base/StringCalculatorShould.java
+++ src/test/java/com/codurance/base/StringCalculatorShould.java
@@ -2,6 +2,7 @@ package com.codurance.base;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class StringCalculatorShould {
@@ -9,6 +10,13 @@ public class StringCalculatorShould {
     @Test
     void return_0_when_empty_string_given() {
         StringCalculator calculator = new StringCalculator();
-        calculator.Add("");
+        assertEquals(0, calculator.Add(""));
+    }
+
+
+    @Test
+    void return_1_when_the_input_string_is_1() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(1, calculator.Add("1"));
     }
 }
